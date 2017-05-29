1c2e4abffd9beea99e5eb1b749ba3d51881631ba
José Fernando Bellucci Cagale <fernando.b.cagale@gmail.com>
2017-05-29 16:07:08 -0300

    add plugin amqp-transport

--- service.js
+++ service.js
@@ -5,6 +5,7 @@ const entity = require('seneca-entity');
 const mongoStore = require('seneca-mongo-store');
 const invoice = require('./lib/invoice');
 const dotenv = require('dotenv');
+const amqp = require('seneca-amqp-transport');
 
 dotenv.load({silent: true});
 
@@ -18,7 +19,12 @@ const opts = {
 seneca.use(entity);
 seneca.use(mongoStore, opts.mongo);
 seneca.use(invoice);
+seneca.use(amqp);
 seneca.listen({
   pin: 'role:invoice,cmd:*',
   port: process.env.PORT || 9003
+}).listen({
+  type: 'amqp',
+  pin: 'role:events,cmd:*',
+  url: process.env.AMQP_URL || 'amqp://localhost'
 });
