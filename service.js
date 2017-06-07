34f950ad5deaeee3820de42d5c19824df5b2bb8b
Fernando Cagale <Fernando Cagale>
2017-06-07 15:04:46 -0300

    add amqp

--- service.js
+++ service.js
@@ -5,7 +5,6 @@ const entity = require('seneca-entity');
 const mongoStore = require('seneca-mongo-store');
 const invoice = require('./lib/invoice');
 const dotenv = require('dotenv');
-const amqp = require('seneca-amqp-transport');
 
 dotenv.load({silent: true});
 
@@ -19,12 +18,7 @@ const opts = {
 seneca.use(entity);
 seneca.use(mongoStore, opts.mongo);
 seneca.use(invoice);
-seneca.use(amqp);
 seneca.listen({
   pin: 'role:invoice,cmd:*',
   port: process.env.PORT || 9003
-}).listen({
-  type: 'amqp',
-  pin: 'role:events,cmd:*',
-  url: process.env.AMQP_URL || 'amqp://localhost'
 });
